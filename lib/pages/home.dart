b24d2b83baf2ae4aca7f8e7a38d43ddab36b3178
arn55 <arnzkiee909@gmail.com>
2023-10-29 07:53:26 +0800

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,36 @@
+ import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+ class HomePage  extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold 
+            ),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+        centerTitle: true,
+        leading: Container(
+          margin:EdgeInsets.all(10),
+          child: SvgPicture.asset('assets/icons/left-arrow.svg',
+          height: 8,
+          width: 8,
+          ),
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)
+          ),
+        )
+      ),
+    );
+  }
+}
